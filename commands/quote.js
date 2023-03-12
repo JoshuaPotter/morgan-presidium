3c01d7ac50f379c7a489177444a632c1f01c14c6
Joshua Potter <joshuaopotter@gmail.com>
2023-03-11 19:23:43 -0500

    Move getLore() to separate lib file

--- commands/quote.js
+++ commands/quote.js
@@ -1,22 +1,4 @@
-import getLoreById from '../lib/lore/getLoreById.js';
-import getLoreByTagName from '../lib/lore/getLoreByTagName.js';
-import getRandomLore from '../lib/lore/getRandomLore.js';
-
-const getLore = async (filter = '') => {
-	const parsedNumber = parseInt(filter);
-
-	let lore = null;
-	if (filter && !isNaN(parsedNumber)) {
-		lore = await getLoreById(parsedNumber);
-	}
-	else if (filter && isNaN(parsedNumber)) {
-		lore = await getLoreByTagName(filter);
-	}
-	else {
-		lore = await getRandomLore();
-	}
-	return lore;
-};
+import getLore from "../lib/lore/getLore";
 
 // Command name
 export const name = "quote";
