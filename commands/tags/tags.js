a673e83e0625004102d3c59148d4c6e7fb3904c7
Joshua Potter <joshuaopotter@gmail.com>
2023-03-14 03:18:40 -0400

    Get tags command

---
+++ commands/tags/tags.js
@@ -0,0 +1,17 @@
+import getAllTags from "../../lib/tags/getAllTags.js";
+
+export const name = "tags";
+
+// Command action
+export async function execute({ command, ack, say }) {
+	// Acknowledge command request
+	await ack({"response_type": "in_channel"});
+
+    const message = await say("See thread for tags.");
+    
+    const tags = await getAllTags();
+    const tagKeys = Object.keys(tags);
+    tagKeys.forEach(async key => {
+        await say({ text: `*Letter:* ${key}\n\`${tags[key].join("` `")}\``, thread_ts: message.ts });
+    });
+};
