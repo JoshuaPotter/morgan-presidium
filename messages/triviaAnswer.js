520fefdc0e4b8b39d09b37ce3ade155876a15281
Joshua Potter <joshuaopotter@gmail.com>
2023-04-10 22:39:36 -0400

    Add response time

--- messages/triviaAnswer.js
+++ messages/triviaAnswer.js
@@ -13,11 +13,11 @@ export async function execute({ message, say }) {
 		}
 
 		// Check if the text is the answer to any active questions. If correct, remove trivia question from the pool and assign points to user.
-		const isCorrect = compareAnswer(text);
-		if (isCorrect) {
+		const comparison = compareAnswer(text);
+		if (comparison.isCorrect) {
 			try {
 				await incrementPoints(user, 10);
-				await say(`:sparkles: *Correct!* <@${user}> received *10 $TNDS*.`);
+				await say(`:sparkles: *Correct (response time: ${comparison.responseTime}s)!* <@${user}> received *10 $TNDS*.`);
 			}
 			catch (error) {
 				console.error(error);
